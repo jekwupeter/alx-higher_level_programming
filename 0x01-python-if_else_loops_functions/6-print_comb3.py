a874aea4cafa42bdae05d3a41a5a075fd2c6ceb4
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-01 00:51:58 +0100

    final correct

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(10):
     for j in range(10):
         if i < j and i + j != 17:
             print("{:d}{:d}".format(i, j), end=", ")
-print(f"{:d}{:d}".format(i - 1, j))
+print("{:d}{:d}".format(i - 1, j))
