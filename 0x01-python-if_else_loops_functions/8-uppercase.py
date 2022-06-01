226c13fdb9a7c84dc7ccf5c4fe501aef8b2b74fa
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-01 09:58:06 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     uppercase - prints a string in uppercase
     @str - input string
     """
-    print(''.join(['{:c}'.format(ord(c) - 32) if ord(c) in range(97, 123) else c
-                  for c in str]))
+    print(''.join(['{:c}'.format(ord(c) - 32) if ord(c) in range(97, 123)
+                  else c for c in str]))
