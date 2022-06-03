555f1eed1683a6866a22c152a7fbdaff5dd73731
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-03 14:45:52 +0100

    Correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
-
-print("{:d} argument:".format(len(sys.argv)))
-for i in range(len(sys.argv)):
-    print("{:d}: {:s}".format(i + 1, sys.argv[i]))
+if __name__ == "__main__"
+    import sys
+    print("{:d} argument:".format(len(sys.argv)))
+    for i in range(len(sys.argv)):
+        print("{:d}: {:s}".format(i + 1, sys.argv[i]))
