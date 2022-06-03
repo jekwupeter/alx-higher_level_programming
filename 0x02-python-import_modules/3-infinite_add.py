bbc824fa05d0830296513284018e36c7a6ca8ee1
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-03 15:12:31 +0100

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-
-sum = 0
-for i in range(len(sys.argv)):
-    sum += int(sys.argv[i])
-    print("{:d}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for i in range(len(sys.argv)):
+        sum += int(sys.argv[i])
+        print("{:d}".format(sum))
