38e3f9cd1dd3b13ce2dd85beb86abcee0240a60c
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-05 00:32:28 +0100

    corrected summation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,12 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for i in range(len(sys.argv)):
-        sum += int(sys.argv[i + 1])
+    arg_list = []
+    if len(sys.argv) == 1:
         print("{:d}".format(sum))
+    else:
+        for i in range(len(sys.argv)):
+            arg_list.append(int(sys.argv[i]))
+            if i != len(sys.argv):
+                arg_list.append(int(sys.argv[i + 1]))
+                print("{:d}".format(sum(arg_list)))
