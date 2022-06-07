eafcf2968ac4b6569e307d2a6b2577342e64f4ab
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-07 01:01:23 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     multiple_returns - Returns string lenght and first char
     @sentence: Input string
     """
-    if instance(sentence, string):
+    if isinstance(sentence, str):
         buffer = [len(sentence), sentence[0]] if len(sentence) != 0 else [len(sentence), None] 
         buffer = tuple(buffer)
         return buffer
