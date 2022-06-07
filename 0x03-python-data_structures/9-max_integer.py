6f4c612fadf7cf08861ffc297374a682a1f8e0c6
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-07 01:08:05 +0100

    Update 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    max_integer - finds biggest integer of a list.
+    @my_list: Input list
+    Return: max integer
+    """
+    if len(my_list) == 0:
+        return None
+
+    temp = 0
+    for x in my_list:
+        temp = x if x > temp
+    return temp
