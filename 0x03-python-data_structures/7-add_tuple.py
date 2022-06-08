2a6a07cc5cecb36301627fad4733f7f7220de468
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-08 14:52:52 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,14 +6,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
     @tuple_b: second input tuple
     """
     result = [0, 0]
-    for i in range(2):
-        if isinstance(tuple_a, tuple):
+    if isinstance(tuple_a, tuple):
+        if len(tuple_a) != 1:
             tuple_a = list(tuple_a) if len(tuple_a) > 1 else [0, 0]
-        elif isinstance(tuple_a, int) or len(tuple_a) == 1:
-            tuple_a = [tuple_a, 0]
-        if isinstance(tuple_b, tuple):
+        else:
+            tuple_a = [tuple_a[0], 0]
+    elif isinstance(tuple_a, int) or len(tuple_a) == 1:
+        tuple_a = [tuple_a, 0]
+    if isinstance(tuple_b, tuple):
+        if len(tuple_b) != 1:
             tuple_b = list(tuple_b) if len(tuple_b) > 1 else [0, 0]
-        elif isinstance(tuple_b, int) or len(tuple_b) == 1:
-            tuple_b = [tuple_b, 0]
-        result[0], result[1] = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+        else:
+            tuple_b = [tuple_b[0], 0]
+    elif isinstance(tuple_b, int) or len(tuple_b) == 1:
+        tuple_b = [tuple_b, 0]
+    result[0], result[1] = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return tuple(result)
