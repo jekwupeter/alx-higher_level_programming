52ff92c524a9a917e24114f3871f336a8987b5c8
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-08 14:06:45 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,11 +5,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
     @tuple_a: first input tuple
     @tuple_b: second input tuple
     """
-    result = []
+    result = [0, 0]
     for i in range(2):
-        if ![tuple_a[i]]:
-            tuple_a[i] = 0
-        if ![tuple_b[i]]:
-            tuple_b[i] = 0
+        if isinstance(tuple_a, tuple):
+            tuple_a = list(tuple_a) if len(tuple_a) > 1 else [0, 0]
+        elif isinstance(tuple_a, int) or len(tuple_a) == 1:
+            tuple_a = [tuple_a, 0]
+            break
+        if isinstance(tuple_b, tuple):
+            tuple_b = list(tuple_b) if len(tuple_b) > 1 else [0, 0]
+        elif isinstance(tuple_b, int) or len(tuple_b) == 1:
+            tuple_b = [tuple_b, 0]
+            break
         result[i] = tuple_a[i] + tuple_b[i]
     return tuple(result)
