e23f0f214cf91bc441c14c30a8960291caf51e9f
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-09 08:42:16 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,5 +6,6 @@ def search_replace(my_list, search, replace):
     @search: element to replace in list
     @replace: the new element
     """
-    my_list = [replace if element == search else element for element in my_list]
+    my_list = [replace if element == search else\
+               element for element in my_list]
     return my_list
