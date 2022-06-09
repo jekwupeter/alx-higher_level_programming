ab436f588ab898b7982f2eaa7a5d38e73e03f37e
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-09 06:36:35 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """
-    multiply_by_2 - checks for the largest value
+    multiply_by_2 - checks for key with the largest value
     @a_dictionary: input dictionary
-    Return: largest value
+    Return: key with largest int value
     """
     if a_dictionary == None or len(a_dictionary) == 0:
         return None
-    largest_value = -9999
-    for i in a_dictionary.values():
-        if i > largest_value:
-            largest_value = i
-    return largest_value
+    largest_value = [-9999, None]
+    for k, v in a_dictionary.items():
+        if int(v) > largest_value[0]:
+            largest_value = [v, k]
+    return largest_value[1]
