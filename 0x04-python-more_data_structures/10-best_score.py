6cedb070af4bcecb49feccecd91d8929315c055e
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-09 07:07:32 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """
-    multiply_list - multiplies each element in iterable by number
-    @my_list: input list
-    @number: number to mult element in list by
-    """
-    mapped_list = list(map(lambda x: x * number, my_list))
-    return mapped_list
+    returnlist(map(lambda x: x * number, my_list))
