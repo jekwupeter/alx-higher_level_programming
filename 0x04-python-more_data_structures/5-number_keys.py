847c7a385c7b87806b6b09222b7686c2af6867d1
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-11 01:44:23 +0100

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -5,7 +5,4 @@ def number_keys(a_dictionary):
     @a_dictionary: input dictionary
     Return: dict key count
     """
-    key_count = 0
-    for i in range(len(a_dictionary.keys())):
-        key_count += i
-    return key_count
+    return (len(a_dictionary))
