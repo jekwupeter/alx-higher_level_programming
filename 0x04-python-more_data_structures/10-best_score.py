bc2601ed66adb5099cb0f7ef0f792b1b67c852a5
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-11 01:53:28 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def best_score(a_dictionary):
+    """
+    number_keys - finds the key with biggest integer
+    @a_dictionary: input dictionary
+    Return: key with biggest integer
+    """
+    if a_dictionary == None or len(a_dictionary) == 0:
+        return None
+
+    buff = -9999
+    for k, v in a_dictionary.items():
+        buff = v if buff < v else buff
+        return k
