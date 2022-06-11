af2a317930f199e2dcedd270fcf054cf3db5a9e1
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-11 20:31:21 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,21 +7,13 @@ def roman_to_int(roman_string):
     """
     result = 0
     single_letter = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    special_case = {"IX": 9, "IV": 4}
     if not isinstance(roman_string, str) or roman_string == None:
         return 0
 
-    if roman_string[-2:] in list(special_case):
-        for char in roman_string[:-2]:
-            for letter, num in single_letter.items():
-                if letter == char:
-                    result += num
-        for i in range(2):
-            if list(special_case)[i] == roman_string[-2:]:
-                result += special_case[list(special_case)[i]]
-    else:
-        for i in roman_string:
-            for letter, num in single_letter.items():
-                if letter == i:
-                    result += num
+    for i in range(len(roman_string)):
+        if i != (len(roman_string) - 1) and single_letter[roman_string[i]] < \
+            single_letter[roman_string[i + 1]]:
+            result += single_letter[roman_string[i]] * -1
+        else:
+            result += single_letter[roman_string[i]]
     return result
