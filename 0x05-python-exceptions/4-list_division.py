2c5cf331a0354203578024af59a66c0616471498
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-21 13:37:36 +0100

    Changed index error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,8 +20,10 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             div = 0
-        except ValueError:
+        except IndexError:
             print("out of range")
+            div = 0
+        except ValueError:
             div = 0
         finally:
             new_list.append(div)
