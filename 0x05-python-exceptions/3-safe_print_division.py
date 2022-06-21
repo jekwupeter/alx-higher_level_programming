ec005797465825790c12a64172210b0052d26099
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-21 13:33:50 +0100

    Added type error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -12,7 +12,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a/b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         pass
     finally:
         print("Inside result:{}".format(result))
