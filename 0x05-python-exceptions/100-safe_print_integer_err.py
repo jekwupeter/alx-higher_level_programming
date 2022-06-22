c4efbf6834bca79a8123dfb8e9962d0ee1324e0f
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-22 03:09:36 +0100

    specified the errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    """prints an integer
+
+    Args:
+        value: input value of any type
+
+    Returns:
+        True of value is inputed correctly else false
+    """
     import sys
     boolean = False
     try:
         print("{:d}".format(value))
         boolean = True
-    except Exception as e:
+    except(ValueError, TypeError) as e:
         print("Exception: ", e, file = sys.stderr)
