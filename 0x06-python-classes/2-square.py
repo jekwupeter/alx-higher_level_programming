4a4f524b648f71cdf9d46d83ec75c1e259362ff4
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-06-23 06:05:16 +0100

    added content

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,15 @@
 
+#!/usr/bin/python3
+"""defines a class square"""
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """Initialization of square class
+        Args:
+            size (int): size of new sqaure
+        """
+        if not isinstance(size, size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
