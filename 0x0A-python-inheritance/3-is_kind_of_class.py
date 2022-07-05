71ba2666bbde8176fa6e848840bb110f3862282a
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-07-05 06:04:16 +0100

    changed shabang

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Module defines a functions that compares class instances"""
 def is_kind_of_class(obj, a_class):
     """checks if object is an instance of a class of\
