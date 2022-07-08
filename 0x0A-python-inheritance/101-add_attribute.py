4fc24435b0ae8ea1f5a8106dacef9a3be5969687
jekwupeter <cephaspeter123@gmail.com>
2022-07-08 12:43:26 +0000

    Task correcton for add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Module define a function that tires to add attribute to a class"""
-def add_attribute(cls, old_a, new_a):
+def add_attribute(cls, attr, attr_value):
     """defines a fucntion that tries to add new attribute to a clas
-    
+
     Args:
         cls (class): input class
-        old_attr (atrr): old attribute of class
-        new_attr (attr): new attribute of class
+        attr: attribute of class
+        attr_value: new attribute value of class
 
     Raises:
-        
+
     """
-    if getattr(cls, old_a):
+    if not hasattr(cls, "__dict__"):
         raise TypeError("can't add new attribute")
 
-    return setattr(cls, old_a, new_a)
+    setattr(cls, attr, attr_value)
