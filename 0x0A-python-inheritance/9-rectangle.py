b207cb1c93048cdee34c832b9cc6d706d32a3d11
jekwupeter <31484984+jekwupeter@users.noreply.github.com>
2022-07-08 02:07:45 +0100

    imported the base class from anotehr moddule

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module define an empty class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 class Rectangle(BaseGeometry):
     """defines a rectangle class that inheirts from another class"""
     def __init__(self, width, height):
